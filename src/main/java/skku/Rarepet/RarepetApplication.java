2c71139500a6231bc5aa2f3eec4db6efe647f6a6
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-04-21 20:47:53 +0900

    first commit

---
+++ src/main/java/skku/Rarepet/RarepetApplication.java
@@ -0,0 +1,15 @@
+package skku.Rarepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RarepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RarepetApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/skku/Rarepet/RarepetApplicationTests.java
@@ -0,0 +1,13 @@
+package skku.Rarepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RarepetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
