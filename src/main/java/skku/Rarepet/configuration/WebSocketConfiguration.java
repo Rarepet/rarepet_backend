14da53555f7b8c52ff469d28b846aab38e7e2b27
HEESOO YANG <didgmltn7769@naver.com>
2022-05-02 19:13:05 +0900

    [EDIT] socketHandler

--- src/main/java/skku/Rarepet/configuration/WebSocketConfiguration.java
+++ src/main/java/skku/Rarepet/configuration/WebSocketConfiguration.java
@@ -1,31 +1,31 @@
 package skku.Rarepet.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import skku.Rarepet.handler.SocketHandler;
 
 import java.io.IOException;
-import java.util.logging.SocketHandler;
+
 
 /**
  * Building the Signaling Server
  */
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
+    private final SocketHandler socketHandler;
     /**
      *  create an endpoint that a client application can use to register as a WebSocket connection
      */
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        try {
-            registry.addHandler((WebSocketHandler) new SocketHandler(), "/socket")
+        registry.addHandler(socketHandler, "/socket")
                     .setAllowedOrigins("*");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
