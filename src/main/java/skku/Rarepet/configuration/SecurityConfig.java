90d1796de9610867a178475b4c9ad6d90bd50bf0
HEESOO YANG <didgmltn7769@naver.com>
2022-05-10 08:21:18 +0900

    [ADD] bcrypto password encoder

--- src/main/java/skku/Rarepet/configuration/SecurityConfig.java
+++ src/main/java/skku/Rarepet/configuration/SecurityConfig.java
@@ -3,16 +3,23 @@ package skku.Rarepet.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
+
+    public PasswordEncoder getArgonPasswordEncoder() {
         return new Argon2PasswordEncoder();
+    }
+
+    @Bean PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
