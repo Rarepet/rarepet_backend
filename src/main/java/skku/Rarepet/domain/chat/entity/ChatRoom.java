da6b06ed8e6a92bf949e212430a320ef8f6cc63f
HEESOO YANG <didgmltn7769@naver.com>
2022-05-11 09:12:38 +0900

    [EDIT] chatroom & message entity

--- src/main/java/skku/Rarepet/domain/chat/entity/ChatRoom.java
+++ src/main/java/skku/Rarepet/domain/chat/entity/ChatRoom.java
@@ -1,6 +1,7 @@
 package skku.Rarepet.domain.chat.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,17 +10,18 @@ import java.util.Collection;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "chat")
-public class Chat {
+@Table(name = "chat_room")
+public class ChatRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "c_id")
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String uuid;
+    private String roomId;
 
     @Column(nullable = false)
     private Long user;
