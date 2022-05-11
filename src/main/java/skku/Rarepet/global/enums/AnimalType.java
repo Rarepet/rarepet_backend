6034caf365c82c98770ef71d8dd4f85cb61e2335
HEESOO YANG <didgmltn7769@naver.com>
2022-05-11 09:13:35 +0900

    [EDIT] animal type enum

--- src/main/java/skku/Rarepet/global/enums/AnimalType.java
+++ src/main/java/skku/Rarepet/global/enums/AnimalType.java
@@ -1,12 +1,17 @@
 package skku.Rarepet.global.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum AnimalType {
-    CAT,
-    DOG,
-    LIZARD,
-    HAMSTER,
-    RABBIT
+    CAT("고양이"),
+    DOG("강아지"),
+    LIZARD("도마뱀"),
+    HAMSTER("햄스터"),
+    RABBIT("토끼"),
+    ;
+
+    private final String type;
 }
